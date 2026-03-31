a1ddb4cd97b1381a6d1c283e10b0535cce58c106
hotwheels1130 <Hotwheels.plays@gmail.com>
2026-03-30 20:08:14 -0500

    changed conf.py

--- source/conf.py
+++ source/conf.py
@@ -14,15 +14,78 @@ release = '0.2'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = [
+    'breathe',
+    'myst_parser',
+    'sphinx_copybutton',
+    'sphinx.ext.autosectionlabel',
+    'sphinx.ext.todo',
+]
 
 templates_path = ['_templates']
-exclude_patterns = []
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+html_theme = "furo"
+
+html_theme_options = {
+    "source_repository": "https://github.com/lemlib/lemlib/",
+    "source_branch": "master",
+    "source_directory": "build/",
+
+    "light_css_variables": {
+        "color-brand-primary": "#00C852",
+        "color-brand-content": "#00C852",
+    },
+
+    "dark_css_variables": {
+        "color-brand-primary": "#00C852",
+        "color-brand-content": "#00C852",
+
+    },
+}
+
 html_static_path = ['_static']
+
+
+breathe_projects = {"Helix": "xml/"}
+
+breathe_projects_source = {
+    "LemLib" : (
+        "../", ["include/lemlib", "include/lemlib/chassis"]
+    )
+}
+
+breathe_default_project = "LemLib"
+
+myst_enable_extensions = [
+    "amsmath",
+    "attrs_inline",
+    "colon_fence",
+    "deflist",
+    "dollarmath",
+    "fieldlist",
+    "html_admonition",
+    "html_image",
+    "linkify",
+    "replacements",
+    "smartquotes",
+    "strikethrough",
+    "substitution",
+    "tasklist",
+]
+
+
+
+# Run doxygen for the readthedocs build
+
+import subprocess, os
+
+read_the_docs_build = os.environ.get('READTHEDOCS', None) == 'True'
+
+if read_the_docs_build:
+     subprocess.call('cd ../doxygen; doxygen', shell=True)
